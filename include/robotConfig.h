1f88627df29882b91e6c86cdce13dd225f1a1cb5
Colby Prescott <crprescott2026@rsu14.org>
2024-05-24 07:28:26 -0400

    Added drivetrain motors to robotConfig

--- include/robotConfig.h
+++ include/robotConfig.h
@@ -5,4 +5,12 @@ extern vex::competition Competition;
 extern vex::brain Brain;
 extern vex::controller ControllerPrimary;
 
+extern motor leftWheelsPrimary;
+extern motor leftWheelsSecondary;
+extern motor_group leftWheels;
+
+extern motor rightWheelsPrimary;
+extern motor rightWheelsSecondary;
+extern motor_group rightWheels;
+
 #endif
--- src/robotConfig.cpp
+++ src/robotConfig.cpp
@@ -7,7 +7,15 @@ competition Competition;
 brain Brain;
 controller ControllerPrimary;
 
+
 // Motors
+motor leftWheelsPrimary = motor(PORT5, ratio18_1, false);
+motor leftWheelsSecondary = motor(PORT6, ratio18_1, true);
+motor_group leftWheels = motor_group(leftWheelsPrimary, leftWheelsSecondary);
+
+motor rightWheelsPrimary = motor(PORT14, ratio18_1, true);
+motor rightWheelsSecondary = motor(PORT16, ratio18_1, false);
+motor_group rightWheels = motor_group(rightWheelsPrimary, rightWheelsSecondary);
 
 
 // Sensors
