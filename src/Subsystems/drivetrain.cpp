34cf182314fd21fe07a35042b5e8d2dcfd1c2e79
Colby Prescott <crprescott2026@rsu14.org>
2024-05-24 11:39:55 -0400

    Added joystick drift check

--- src/Subsystems/drivetrain.cpp
+++ src/Subsystems/drivetrain.cpp
@@ -5,6 +5,8 @@ using namespace vex;
 float driveSpeed = 1;
 float turnSpeed = 0.4;
 
+float driftThreshold = 0.08;
+
 void InitDrivetrain() {
 
 }
@@ -17,6 +19,10 @@ void TickDrivetrain() {
     // Get controller input
     float forwardInput = PrimaryController.Axis3.value() / 127;
     float rightwardInput = PrimaryController.Axis1.value() / 127;
+
+    // Nullify input if it could be drifting
+    if(fabs(forwardInput) < driftThreshold) forwardInput = 0;
+    if(fabs(rightwardInput) < driftThreshold) rightwardInput = 0; 
 
     // Apply speed
     forwardInput *= driveSpeed;
