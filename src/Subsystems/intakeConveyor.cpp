6e66731074f9fb948f7021ef30d32ffe7535f3a4
Colby Prescott <crprescott2026@rsu14.org>
2024-05-31 14:30:57 -0400

    Fixed intakeConveyorDirection that was bool instead of int

--- src/Subsystems/intakeConveyor.cpp
+++ src/Subsystems/intakeConveyor.cpp
@@ -2,23 +2,23 @@
 
 using namespace vex;
 
-const double intakeConveyorSpeed = 0.5;
+const double intakeConveyorSpeed = 0.4;
 
-bool intakeConveyorDirection = 0;
+int intakeConveyorDirection = 0;
 
 void InitIntakeConveyor() {
     PrimaryController.ButtonUp.pressed([](){
         // If already moving forwards, stop
         if(intakeConveyorDirection == 1) {intakeConveyor.stop(); intakeConveyorDirection = 0;}
         // If stopped or moving backwards, spin forwards
-        else {intakeConveyor.spin(forward, fmin(fmax(intakeConveyorDirection * 12, -12), 12), volt); intakeConveyorDirection = 1;}
+        else {intakeConveyor.spin(forward, fmin(fmax(intakeConveyorSpeed * 12, -12), 12), volt); intakeConveyorDirection = 1;}
     });
 
     PrimaryController.ButtonDown.pressed([](){
         // If already moving backwards, stop
         if(intakeConveyorDirection == -1) {intakeConveyor.stop(); intakeConveyorDirection = 0;}
         // If stopped or moving forwards, spin backwards
-        else {intakeConveyor.spin(forward, fmin(fmax(-intakeConveyorDirection * 12, -12), 12), volt); intakeConveyorDirection = -1;}
+        else {intakeConveyor.spin(reverse, fmin(fmax(intakeConveyorSpeed * 12, -12), 12), volt); intakeConveyorDirection = -1;}
     });
 }
 
