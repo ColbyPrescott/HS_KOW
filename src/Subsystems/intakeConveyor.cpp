9ad903cc0b872a86828d8a1ea8fefade824f53f4
Colby Prescott <crprescott2026@rsu14.org>
2024-07-18 13:44:50 -0400

    Test slowing down intake
    Pressing left arrow will decrease the speed of the intake conveyor.
    Full speed is good for pushing rings onto the alliance stake, but it can overshoot mobile goal stakes. Perhaps slowing it down could fit that?

--- src/Subsystems/intakeConveyor.cpp
+++ src/Subsystems/intakeConveyor.cpp
@@ -2,7 +2,7 @@
 
 using namespace vex;
 
-const double intakeConveyorSpeed = 0.4;
+const double intakeConveyorSpeed = 0.8;
 
 int intakeConveyorDirection = 0;
 
@@ -19,6 +19,10 @@ void InitIntakeConveyor() {
         if(intakeConveyorDirection == -1) {intakeConveyor.stop(); intakeConveyorDirection = 0;}
         // If stopped or moving forwards, spin backwards
         else {intakeConveyor.spin(reverse, fmin(fmax(intakeConveyorSpeed * 12, -12), 12), volt); intakeConveyorDirection = -1;}
+    });
+    PrimaryController.ButtonLeft.pressed([](){
+        if(intakeConveyorDirection == 1) intakeConveyor.spin(forward, fmin(fmax(intakeConveyorSpeed * 0.5 * 12, -12), 12), volt);
+        if(intakeConveyorDirection == -1) intakeConveyor.spin(reverse, fmin(fmax(intakeConveyorSpeed * 0.5 * 12, -12), 12), volt);
     });
 }
 
