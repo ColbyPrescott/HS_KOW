d9cd0f9856d916d3d726f79e4b0c8d0588d49029
Colby Prescott <crprescott2026@rsu14.org>
2024-07-22 12:02:16 -0400

    Change ports
    Moved many ports around.
    Attatched the brain in a different location.

--- src/robotConfig.cpp
+++ src/robotConfig.cpp
@@ -3,27 +3,29 @@
 using namespace vex;
 
 // VEX related global variables
+// I would love to use camelCase here like all of my other variables, but 
+// VEX uses a different naming convention that would create naming conflicts
 competition Competition;
 brain Brain;
 controller PrimaryController;
 
 
 // Motors
-motor leftWheelsPrimary = motor(PORT5, ratio18_1, false);
-motor leftWheelsSecondary = motor(PORT6, ratio18_1, true);
+motor leftWheelsPrimary = motor(PORT15, ratio18_1, false);
+motor leftWheelsSecondary = motor(PORT20, ratio18_1, true);
 motor_group leftWheels = motor_group(leftWheelsPrimary, leftWheelsSecondary);
 
-motor rightWheelsPrimary = motor(PORT14, ratio18_1, true);
-motor rightWheelsSecondary = motor(PORT16, ratio18_1, false);
+motor rightWheelsPrimary = motor(PORT5, ratio18_1, true);
+motor rightWheelsSecondary = motor(PORT9, ratio18_1, false);
 motor_group rightWheels = motor_group(rightWheelsPrimary, rightWheelsSecondary);
 
-motor intakeConveyorTop = motor(PORT1, ratio18_1, false);
+motor intakeConveyorTop = motor(PORT8, ratio18_1, false);
 motor intakeConveyorBottom = motor(PORT2, true);
 motor_group intakeConveyor = motor_group(intakeConveyorTop, intakeConveyorBottom);
 
 
 // Pneumatics
-pneumatics mogoMover = pneumatics(Brain.ThreeWirePort.A);
+pneumatics mogoMover = pneumatics(Brain.ThreeWirePort.H);
 pneumatics intakeProp = pneumatics(Brain.ThreeWirePort.B);
 
 
