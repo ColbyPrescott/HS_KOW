f40ea96b9aedd1857bb674b3ada82bb931e7646b
Colby Prescott <crprescott2026@rsu14.org>
2024-07-29 09:35:01 -0400

    Test intake checkpoints
    Tried creating a system where pressing the right button would increment the intake to a next checkpoint. Currently doesn't wrap around properly.
    Most likely, the lift will work in the future such that it waits for a ring to pickup.

--- src/Subsystems/intakeConveyor.cpp
+++ src/Subsystems/intakeConveyor.cpp
@@ -6,6 +6,29 @@ using namespace vex;
 // Speed for the intake conveyor motors, 0 - 1
 const double intakeConveyorRPM = 200;
 
+enum class IntakeDegreePositions {
+    lowest = 0, // Start encoder with standoffs at lowest possible position
+    highest = 2500, // Standoffs at highest possible position
+    startHorizontal = 2583, // Standoffs round the corner and now face perfectly horizontal
+    endHorizontal = 2800, // Last point where standoffs are horizontal
+    startPlexiglass = 3720, // Standoffs have rounded the corner and now start to touch the plexiglass
+    endPlexiglass = 4400, // Standoffs cleared the plexiglass and are now back to normal orientation
+    readyForDisk = 4600, // Standoff is at a good position to quickly pick up the next disk
+    lowestReset = 5040, // Standoff is at the lowest point again, having traveled a full entire loop
+};
+
+int intakeDegreePositions[] = {
+    (int)IntakeDegreePositions::lowest,
+    (int)IntakeDegreePositions::highest,
+    (int)IntakeDegreePositions::startHorizontal,
+    (int)IntakeDegreePositions::endHorizontal,
+    (int)IntakeDegreePositions::startPlexiglass,
+    (int)IntakeDegreePositions::endPlexiglass,
+    (int)IntakeDegreePositions::readyForDisk,
+};
+int intakeDegreePositionIndex = 0;
+const int numIntakeDegreePositions = 7;
+
 
 // Variables needed for system functionality
 // Direction of the intake conveyor motors. -1 is backwards, 0 is stopped, and 1 is forwards
@@ -36,6 +59,28 @@ void InitIntakeConveyor() {
         if(intakeConveyorDirection == 1) intakeConveyor.spin(forward, intakeConveyorRPM * 0.5, rpm);
         if(intakeConveyorDirection == -1) intakeConveyor.spin(reverse, intakeConveyorRPM * 0.5, rpm);
     });
+
+    PrimaryController.ButtonRight.pressed([](){
+        intakeDegreePositionIndex++;
+        if(intakeDegreePositionIndex == numIntakeDegreePositions) {
+            intakeDegreePositionIndex = 0;
+            int oldEncoder = intakeConveyor.position(degrees);
+            int resetOffset = intakeDegreePositions[numIntakeDegreePositions - 1];
+            intakeConveyor.setPosition(oldEncoder - resetOffset, degrees);
+        }
+        printf("At: %i, Spinning to: %i\n", (int)intakeConveyor.position(degrees), intakeDegreePositions[intakeDegreePositionIndex]); // 4319 to 4400, 4399 to 4600, 0 to 0 when it should be -440 to 0
+        intakeConveyor.spinToPosition(intakeDegreePositions[intakeDegreePositionIndex], degrees, intakeConveyorRPM, rpm, false);
+
+        // // intakeConveyor.setPosition(0, degrees);
+        // printf("%f\n", intakeConveyor.position(degrees));
+        // intakeConveyor.setPosition(100, degrees);
+        // printf("%f\n", intakeConveyor.position(degrees));
+        // intakeConveyor.setPosition(720, degrees);
+        // printf("%f\n", intakeConveyor.position(degrees));
+        // intakeConveyor.setPosition(-100, degrees);
+        // printf("%f\n", intakeConveyor.position(degrees));
+    });
+
 }
 
 // Initialize intake conveyor at the start of driver control
