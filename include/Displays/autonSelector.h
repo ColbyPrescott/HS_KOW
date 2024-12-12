22f0ab4fe29bdaa60dd79c00bbb93cfebdcd6fbb
Colby Prescott <colbyrprescott@gmail.com>
2024-12-12 07:47:40 -0500

    Update auton selector GUI to display the current value of selectedAutonSequence
    Added some new text in autonSelector.cpp and also rearranged the buttons into a grid.
    The currently selected autonomous text will reassure that a button was clicked, and the grid will make it easier to add and remove buttons in the future.

--- include/Displays/autonSelector.h
+++ include/Displays/autonSelector.h
@@ -1,8 +1,6 @@
 #ifndef AUTONOMOUS_SELECTOR_H
 #define AUTONOMOUS_SELECTOR_H
 
-#include "KOWGUI/Nodes/Basic/group.h"
-
 extern void (*selectedAutonSequence)();
 
 #endif
--- src/Displays/autonSelector.cpp
+++ src/Displays/autonSelector.cpp
@@ -11,36 +11,54 @@ void (*selectedAutonSequence)() = nullptr;
 // Initialize the UI for the autonomous selection screen
 void InitScreenAutonSelector() {
     screens.autonSelector->AddChildren({
-        // Button to select AutonNegativeRed
-        (new Clickable)->SetPosition(30, 50)->SetSize(200, 50)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonNegativeRed;})->AddChildren({
-            (new NSelected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::red)}),
-            (new Selected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkRed)}),
-            (new Text)->SetText("- Red")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
-        }),
-        // Button to select AutonNegativeBlue
-        (new Clickable)->SetPosition(250, 50)->SetSize(200, 50)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonNegativeBlue;})->AddChildren({
-            (new NSelected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::blue)}),
-            (new Selected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkBlue)}),
-            (new Text)->SetText("- Blue")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
-        }),
-        // Button to select AutonPositiveRed
-        (new Clickable)->SetPosition(30, 130)->SetSize(200, 50)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonPositiveRed;})->AddChildren({
-            (new NSelected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::red)}),
-            (new Selected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkRed)}),
-            (new Text)->SetText("+ Red")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
-        }),
-        // Button to select AutonPositiveBlue
-        (new Clickable)->SetPosition(250, 130)->SetSize(200, 50)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonPositiveBlue;})->AddChildren({
-            (new NSelected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::blue)}),
-            (new Selected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkBlue)}),
-            (new Text)->SetText("+ Blue")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+        (new Column)->SetPosition(30, 50)->SetSize(480 - 60, 240 - 50 - 30)->SetScaleToFit(true)->SetSpacing(10)->AddChildren({
+            (new Row)->SetScaleToFit(true)->SetSpacing(30)->AddChildren({
+                // Button to select AutonNegativeRed
+                (new Clickable)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonNegativeRed;})->AddChildren({
+                    (new NFocused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::red)}),
+                    (new Focused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkRed)}),
+                    (new Text)->SetText("- Red")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+                }),
+                // Button to select AutonNegativeBlue
+                (new Clickable)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonNegativeBlue;})->AddChildren({
+                    (new NFocused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::blue)}),
+                    (new Focused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkBlue)}),
+                    (new Text)->SetText("- Blue")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+                }),
+            }),
+            (new Row)->SetScaleToFit(true)->SetSpacing(30)->AddChildren({
+                // Button to select AutonPositiveRed
+                (new Clickable)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonPositiveRed;})->AddChildren({
+                    (new NFocused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::red)}),
+                    (new Focused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkRed)}),
+                    (new Text)->SetText("+ Red")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+                }),
+                // Button to select AutonPositiveBlue
+                (new Clickable)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonPositiveBlue;})->AddChildren({
+                    (new NFocused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::blue)}),
+                    (new Focused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::darkBlue)}),
+                    (new Text)->SetText("+ Blue")->SetFontSize(30)->SetColor(Color::white)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+                }),
+            }),
+
+            // Button to select AutonSkills
+            (new Clickable)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonSkills;})->AddChildren({
+                (new NFocused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::white)}),
+                (new Focused)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::gray)}),
+                (new Text)->SetText("Skills")->SetFontSize(30)->SetColor(Color::black)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+            }),
         }),
 
-        // Button to select AutonSkills
-        (new Clickable)->SetPosition(0, 0)->SetSize(40, 40)->SetRelease([](BaseNode* self){selectedAutonSequence = AutonSkills;})->AddChildren({
-            (new NSelected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::white)}),
-            (new Selected)->AddChildren({(new RoundedRectangle)->SetOutlineRadius(30)->SetFillColor(Color::gray)}),
-            (new Text)->SetText("S")->SetFontSize(30)->SetColor(Color::black)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::wrap),
+        // Text to show the current selection
+        (new Text)->SetID("selectedAutonText")->SetPosition(240, 0)->SetSize(50, 50)->SetFontSize(35)->SetFont(Text::fontProportional)->SetAlignments(Text::HorizontalAlign::center, Text::VerticalAlign::middle)->SetOverflow(Text::Overflow::visible)->SetPreTick([](BaseNode* self){
+            Text* textSelf = (Text*)self;
+            if(selectedAutonSequence == nullptr) textSelf->SetText("Selected: None");
+            else if(selectedAutonSequence == AutonNegativeRed) textSelf->SetText("Selected: - Red");
+            else if(selectedAutonSequence == AutonNegativeBlue) textSelf->SetText("Selected: - Blue");
+            else if(selectedAutonSequence == AutonPositiveRed) textSelf->SetText("Selected: + Red");
+            else if(selectedAutonSequence == AutonPositiveBlue) textSelf->SetText("Selected: + Blue");
+            else if(selectedAutonSequence == AutonSkills) textSelf->SetText("Selected: Skills");
+            else textSelf->SetText("Selected: ERROR");
         }),
     });
 }
